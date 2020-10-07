7865d4077c601bae731b2f949337144baa26c7c1
maTh51 <matheustigon@gmail.com>
2020-10-07 14:15:46 -0300

    Commit #5: Dollar and Franc classes removed

--- src/test/java/Testes.java
+++ src/test/java/Testes.java
@@ -4,10 +4,6 @@
  * and open the template in the editor.
  */
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,59 +12,37 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author matheus
  */
 
-abstract class Money  {
+class Money  {
     protected int amount;
     private String currency;
-   
-    public boolean equals(Object object)  {
-        Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
-    }
 
     static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
+      return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-       return new Franc(amount, "CHF");
+       return new Money(amount, "CHF");
     }
 
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
-	
-    abstract Money times(int multiplier);
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+    
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
 
     String currency() {
         return currency;
     }
    
 }
-
-class Dollar extends Money {
-    
-    Dollar(int amount, String currency)  {
-      super(amount, currency);
-    }
-
-    Money times(int multiplier)  {
-       return Money.dollar(amount * multiplier);
-    }
- }	
-
-class Franc extends Money{
-   
-    Franc(int amount, String currency) {
-      super(amount, currency);
-    }
-
-    Money times(int multiplier)  {
-       return Money.franc(amount * multiplier);
-    }
-
-}
-
 
 public class Testes {
     
@@ -83,19 +57,10 @@ public class Testes {
     
     @Test
     public void testEquality() {
-        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-    
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-     } 
+    } 
     
     @Test
     public void testCurrency() {
