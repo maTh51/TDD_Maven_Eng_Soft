a688e1ec30d8bcd65a29ec515d0a93efc3b95df0
maTh51 <matheustigon@gmail.com>
2020-10-07 12:35:39 -0300

    Commit #2: Adding Money and Franc classes

--- src/test/java/Testes.java
+++ src/test/java/Testes.java
@@ -16,11 +16,35 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author matheus
  */
 
-class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
+class Money  {
+   protected int amount;
+   
+   public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return amount == money.amount;
+   }   
+}
+
+class Dollar extends Money {
+    Dollar(int amount){ 
+        this.amount = amount;
+    }			
+    
+    Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
+    }
+    
  }	
+
+class Franc extends Money{   
+   
+   Franc(int amount) {      
+      this.amount= amount;
+    }					
+    Franc times(int multiplier)  {      
+       return new Franc(amount * multiplier);					
+    }   								
+}
 
 
 public class Testes {
@@ -30,11 +54,26 @@ public class Testes {
 
     @Test
     public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
-
+    
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+    
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }   
+    
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
